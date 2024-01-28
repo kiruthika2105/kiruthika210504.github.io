8237e299f30d6742e87cbbebcd735c8808418d68
KIRUTHIKA <86547441+kiruthika2105@users.noreply.github.com>
2024-01-28 17:07:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:[" Creative Frontend Developer...","Fresher...","Web Designer..."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
